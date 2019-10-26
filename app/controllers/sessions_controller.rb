f9ba15b66e6f9e984e5bb69b62a61f92b4d3cb23
ashok997 <ashok997@gmail.com>
2019-10-26 10:32:19 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def create
+    if params[:name].present?
+        session[:name] = params[:name]
+        redirect_to '/'
+      else
+        redirect_to :login
+    end
+  end
+
+  def destroy
+    if session[:name].present?
+        session.delete :name
+    end
+  end
+
+end
